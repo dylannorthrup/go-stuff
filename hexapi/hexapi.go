292ea1285be6457cf5b4c764386c1a41112bd399
Dylan Northrup <dylan.northrup@turner.com>
2015-08-07 10:35:12 -0400

    Lots of updates around drafting

--- hexapi/hexapi.go
+++ hexapi/hexapi.go
@@ -3,11 +3,11 @@ package main
 //  Things needed to do
 //  + Parse JSON into something I can do stuff with
 //  + Read in config values from a config file
-//  - Get price data from URL
+//  + Get price data from URL
 //  - Handle the following types of events
-//    - Login
-//    - Logout
-//    - DraftPack
+//    + Login
+//    + Logout
+//    + DraftPack
 //    - DraftCardPicked/DaraftCardPicked
 //    - GameStarted
 //    - GameEnded
@@ -60,14 +60,17 @@ type sortedCardCollection struct {
 var scc sortedCardCollection
 
 // Configuration values we'll use all around
-//type Config map[string]string
 var Config = make(map[string]string)
 
 // And some general variables we'll use to keep track of things
 var GameStartTime time.Time = time.Now()
+var packValue int = 0
+var packCost int = 0
+var draftCardsPicked map[string]int = make(map[string]int)
 
 // FUNCTIONS
 
+// Something to print out details of our collection for all cards we have at least 1 of
 func printCollection() {
 	// Make map of names to uuids
 	for _, c := range cardCollection {
@@ -82,7 +85,9 @@ func printCollection() {
 		nmk[n] = k
 		n++
 	}
+	// Sort that array
 	sort.Strings(nmk)
+	// Then use that array to print out card info in Alphabetical order
 	for _, name := range nmk {
 		uuid := ntum[name]
 		entry := cardCollection[uuid]
@@ -90,16 +95,13 @@ func printCollection() {
 			printCardInfo(entry)
 		}
 	}
-	//	for _, entry := range cardCollection {
-	//		if entry.qty > 0 {
-	//			printCardInfo(entry)
-	//		}
-	//	}
 }
 
-func cardUpdatedEvent() {
+func cardUpdatedEvent(f map[string]interface{}) {
+	fmt.Printf("CardUpdatedEvent: %v\n", f)
 }
 
+// Modify card quantities
 func incrementCardCount(uuid string) {
 	changeCardCount(uuid, 1)
 }
@@ -107,61 +109,170 @@ func decrementCardCount(uuid string) {
 	changeCardCount(uuid, -1)
 }
 func changeCardCount(uuid string, i int) {
+	// Check to see if we've got an entry in draftCardsPicked we need to account for
+	if _, ok := draftCardsPicked[uuid]; ok {
+		if draftCardsPicked[uuid] > 0 {
+			decrementDraftCardsPicked(uuid)
+			return
+		}
+	}
 	if _, ok := cardCollection[uuid]; ok {
 		c := cardCollection[uuid]
 		c.qty += i
+		fmt.Printf("New qty for '%v' is %v (modified by %v)\n", c.name, c.qty, i)
 		cardCollection[uuid] = c
 	}
 }
-
-func printCardInfo(c Card) {
-	fmt.Printf("'%v' [Qty: %v] - %vp and %vg\n", c.name, c.qty, c.plat, c.gold)
+func incrementDraftCardsPicked(uuid string) {
+	changeDraftCardsCount(uuid, 1)
+}
+func decrementDraftCardsPicked(uuid string) {
+	changeDraftCardsCount(uuid, -1)
+}
+func changeDraftCardsCount(uuid string, i int) {
+	if _, ok := draftCardsPicked[uuid]; ok {
+		draftCardsPicked[uuid] += i
+	} else {
+		draftCardsPicked[uuid] = 0
+		draftCardsPicked[uuid] += i
+	}
+	// Make sure we don't go negative
+	if draftCardsPicked[uuid] < 0 {
+		draftCardsPicked[uuid] = 0
+	}
 }
 
+// Show us relevant info about the cards
+func printCardInfo(c Card) {
+	s := getCardInfo(c)
+	fmt.Printf("%v\n", s)
+}
+func getCardInfo(c Card) string {
+	return fmt.Sprintf("'%v' [Qty: %v] - %vp and %vg", c.name, c.qty, c.plat, c.gold)
+}
+
+// Return the UUID from a big of JSON
+func getCardUUID(f map[string]interface{}) string {
+	uuid_map := f["Guid"].(map[string]interface{})
+	uuid := uuid_map["m_Guid"].(string)
+	return uuid
+}
+
+// Handle picking of Draft Cards
+// Immediately increment the count of the card, but also keep track of this
+// so we can adjust later when the Collection Update events come in
+func draftCardPickedEvent(f map[string]interface{}) {
+	card := f["Card"].(map[string]interface{})
+	uuid := getCardUUID(card)
+	incrementCardCount(uuid)
+	incrementDraftCardsPicked(uuid)
+}
+
+// Process draft pack choices
+func draftPackEvent(f map[string]interface{}) {
+	haveLeastOf := Card{name: "bogusvalue"}
+	worthMostGold := Card{name: "bogusvalue"}
+	worthMostPlat := Card{name: "bogusvalue"}
+	cards, _ := f["Cards"].([]interface{})
+	if len(cards) == 15 {
+		packValue = 0
+	}
+	for _, u := range cards {
+		card := u.(map[string]interface{})
+		uuid := getCardUUID(card)
+		c := cardCollection[uuid]
+		// If this is the first time through, these will be nil
+		if haveLeastOf.name == "bogusvalue" {
+			haveLeastOf = c
+		}
+		if worthMostGold.name == "bogusvalue" {
+			worthMostGold = c
+		}
+		if worthMostPlat.name == "bogusvalue" {
+			worthMostPlat = c
+		}
+		haveLeastOf = leastQty(haveLeastOf, c)
+		worthMostGold = mostGold(worthMostGold, c)
+		worthMostPlat = mostPlat(worthMostPlat, c)
+	}
+	mostGold := getCardInfo(worthMostGold)
+	mostPlat := getCardInfo(worthMostPlat)
+	haveLeast := getCardInfo(haveLeastOf)
+	fmt.Printf("Worth most gold: %v\n", mostGold)
+	fmt.Printf("Worth most plat: %v\n", mostPlat)
+	fmt.Printf("Have least of: %v\n", haveLeast)
+	// Now that we've done the comparison, print out our ROI for the pack
+	if len(cards) == 1 {
+		packValue += worthMostPlat.plat
+		fmt.Println("Total pack value: %v and pack profit is %v", packValue, packValue-packCost)
+	}
+}
+
+// Comparison functions between cards
+// We use '>' and '<' to favor cards of higher rarity if/when there's a tie since they show up later
+// in the packs (as they are right now)
+func leastQty(c1, c2 Card) Card {
+	if c1.qty < c2.qty {
+		return c1
+	}
+	return c2
+}
+
+func mostGold(c1, c2 Card) Card {
+	if c1.gold > c2.gold {
+		return c1
+	}
+	return c2
+}
+
+func mostPlat(c1, c2 Card) Card {
+	if c1.plat > c2.plat {
+		return c1
+	}
+	return c2
+}
+
+// Process Collection Event
 func collectionEvent(f map[string]interface{}) {
 	action := f["Action"]
 	added, _ := f["CardsAdded"].([]interface{})
 	removed, _ := f["CardsRemoved"].([]interface{})
-	switch action {
-	case "Overwrite":
+	if action == "Overwrite" {
 		fmt.Printf("Got an Overwrite Collection message\n")
 		// First thing we do is reset counts on all cards
 		for k, v := range cardCollection {
 			v.qty = 0
 			cardCollection[k] = v
 		}
-		// Ok, let's extract the cards and update the numbers of each card, then cache that locally (in case we
-		// need to restart for some reason)
-		for _, u := range added {
-			card := u.(map[string]interface{})
-			uuid_map := card["Guid"].(map[string]interface{})
-			uuid := uuid_map["m_Guid"].(string)
-			incrementCardCount(uuid)
-		}
-		// For DEBUGGING
-		printCollection()
-	case "Update":
-		fmt.Printf("Got an Update Collection message\nCards removed: %v\n", removed)
-	default:
-		fmt.Printf("Got an unknown Collection Action '%v'\n", action)
 	}
-}
-
-func draftCardPickedEvent() {
-}
-
-func draftPackEvent() {
+	// Ok, let's extract the cards and update the numbers of each card.
+	// TODO: cache that locally (in case we need to restart for some reason)
+	for _, u := range added {
+		card := u.(map[string]interface{})
+		uuid := getCardUUID(card)
+		incrementCardCount(uuid)
+	}
+	for _, u := range removed {
+		card := u.(map[string]interface{})
+		uuid := getCardUUID(card)
+		decrementCardCount(uuid)
+	}
+	// For DEBUGGING
+	//printCollection()
 }
 
 // Message: {"Winners":["Uzume, Grand Concubunny"],"Losers":["Warmaster Fuzzuko"],"User":"InGameName","Message":"GameEnded"}
-func gameEndedEvent() {
+func gameEndedEvent(f map[string]interface{}) {
 	elapsed := time.Since(GameStartTime)
-	fmt.Printf("Elapsed Game Time: %s", elapsed)
+	winners := f["Winners"].([]interface{})
+	losers := f["Losers"].([]interface{})
+	fmt.Printf("%v triumphed over %v in an elapsed time of %s\n", winners[0], losers[0], elapsed)
 }
 
 // Message: {"Players":[],"User":"InGameName","Message":"GameStarted"}
 func gameStartedEvent() {
 	GameStartTime = time.Now()
+	fmt.Printf("Game started at %v\n", GameStartTime)
 }
 
 // {"User":"","Message":"Login"} - Logging in from new start of Hex
@@ -190,7 +301,7 @@ func incoming(rw http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic("AIEEE: Could not readAll for req.Body")
 	}
-	fmt.Println("REPLY BODY: ", string(body))
+	//	fmt.Println("REPLY BODY: ", string(body))
 	//err = json.Unmarshal(body, &t)
 	var f map[string]interface{}
 	err = json.Unmarshal(body, &f)
@@ -198,26 +309,26 @@ func incoming(rw http.ResponseWriter, req *http.Request) {
 		panic("AIEEE: Could not Unmarshall the body")
 	}
 	//	fmt.Println("DEBUG: Unmarshall successful")
-	fmt.Println("DEBUG: Message is", f["Message"])
+	//	fmt.Println("DEBUG: Message is", f["Message"])
 	msg := f["Message"]
 	switch msg {
 	case "CardUpdated":
-		fmt.Printf("Got a Card Updated message\n")
-		cardUpdatedEvent()
+		//		fmt.Printf("Got a Card Updated message\n")
+		cardUpdatedEvent(f)
 	case "Collection":
-		fmt.Printf("Got a Collection message\n")
+		//		fmt.Printf("Got a Collection message\n")
 		collectionEvent(f)
 	case "DraftCardPicked":
-		fmt.Printf("Got a Draft Card Picked message\n")
-		draftCardPickedEvent()
+		//		fmt.Printf("Got a Draft Card Picked message\n")
+		draftCardPickedEvent(f)
 	case "DraftPack":
-		fmt.Printf("Got a Draft Pack message\n")
-		draftPackEvent()
+		//		fmt.Printf("Got a Draft Pack message\n")
+		draftPackEvent(f)
 	case "GameEnded":
-		fmt.Printf("Got a Game Ended message\n")
-		gameEndedEvent()
+		//		fmt.Printf("Got a Game Ended message\n")
+		gameEndedEvent(f)
 	case "GameStarted":
-		fmt.Printf("Got a Game Started message\n")
+		//		fmt.Printf("Got a Game Started message\n")
 		gameStartedEvent()
 	case "Login":
 		//		fmt.Printf("Got a Login message\n")
@@ -324,6 +435,7 @@ func getCardPriceInfo() {
 	s := string(body)
 	lines := strings.Split(s, "\n")
 	re, err := regexp.Compile(`^(.*?) \.\.\. ([\d\w-]+) \.\.\. (\d+) PLATINUM.* \.\.\. (\d+) GOLD`)
+	fmt.Println("Processing price data")
 	// We skip the first line since it's a header line
 	for _, line := range lines[1:] {
 		result := re.FindStringSubmatch(line)
@@ -332,7 +444,7 @@ func getCardPriceInfo() {
 			uuid := result[2]
 			plat, _ := strconv.Atoi(result[3])
 			gold, _ := strconv.Atoi(result[4])
-			fmt.Printf("Name: %v - %v - %vp, %vg\n", name, uuid, plat, gold)
+			//			fmt.Printf("Name: %v - %v - %vp, %vg\n", name, uuid, plat, gold)
 			// We test to see if this uuid is already in our collection. If so, update it
 			if _, ok := cardCollection[uuid]; ok {
 				// We can't update directly, so we create a new card, modify it's values, then reassign it back to
@@ -352,13 +464,18 @@ func getCardPriceInfo() {
 			}
 		}
 	}
+	// Grab out the draft pack price here
+	draftPack := cardCollection["draftpak-0000-0000-0000-000000000000"]
+	packCost = draftPack.plat
+	// And now let them know we're ready
+	fmt.Println("Beginning to listen for API events")
 }
 
 func main() {
 	// Read config file
 	Config = loadDefaults()
 	Config = readConfig("config.ini", Config)
-	fmt.Printf("Using the following configuration values\n\tPrice URL (price_url): '%v'\n\tCollection file (collection_file): '%v'\n\tAlternate Art/Promo List URL(aa_promo_url): '%v'\n", Config["price_url"], Config["collection_file"], Config["aa_promo_url"])
+	//	fmt.Printf("Using the following configuration values\n\tPrice URL (price_url): '%v'\n\tCollection file (collection_file): '%v'\n\tAlternate Art/Promo List URL(aa_promo_url): '%v'\n", Config["price_url"], Config["collection_file"], Config["aa_promo_url"])
 	// Retrieve card price info
 	getCardPriceInfo()
 	// Register http handlers before starting the server
