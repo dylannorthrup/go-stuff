bf7f77456d652239c10df36123ed55e98ae895d3
Dylan Northrup <dylan.northrup@turner.com>
2016-09-19 18:23:36 -0400

    Initial

---
+++ hexapi/retag.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Retag the project when it's ready for release
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+VERS=$(awk -F\" '/var programVersion/ {print $2}' hexapi/hexapi.go)
+
+echo "Tagging version ${VERS}"
+git tag ${VERS}
+git push --tags
+
+echo "Removing current and stable tags and re-pointing them to this commit"
+git tag -d stable
+git push origin :refs/tags/stable
+git tag -d current
+git push origin :refs/tags/current
+git tag stable
+git tag current
