3c11dee634268ac293ee9b2b8110f90ab2f81722
Dylan Northrup <dylan.northrup@turner.com>
2016-10-04 16:50:42 -0400

    Added tests for card counting

--- hexapi/hexapi_test.go
+++ hexapi/hexapi_test.go
@@ -16,6 +16,9 @@ func TestValuesDiffer(t *testing.T) {
 	} {
 		// a := 1
 		// b := 1
+		Config = make(map[string]string)
+		Config["running_test"] = "true"
+
 		got := valuesDiffer(c.a, c.b)
 		if got != c.want {
 			t.Errorf("valuesDiffer(%q, %q) == %v but we expected %v", c.a, c.b, got, c.want)
@@ -37,5 +40,81 @@ func TestFloatToInt(t *testing.T) {
 		if got != c.want {
 			t.Errorf("floatToInt(%v) == %v but we expected %v", c.a, got, c.want)
 		}
+	}
+}
+
+func TestObjRawChangeCardCount(t *testing.T) {
+	uuid := "ff9f4b37-6b97-4cc6-bbde-87974f1bb678"
+	for _, nums := range []struct {
+		a int
+	}{
+		{1}, {2}, {0}, {100},
+	} {
+		c := Card{name: "Test Card", uuid: uuid, plat: 1, gold: 1, rarity: "Epic", qty: 0, nature: "Smelly"}
+		c.objRawChangeCardCount(nums.a)
+		q := c.qty
+		if q != nums.a {
+			t.Errorf("c.rawChangeCardCount(%v) == %v but we expected %v", nums.a, c.qty, nums.a)
+		}
+
+	}
+}
+
+func TestRawChangeCardCount(t *testing.T) {
+	uuid := "ff9f4b37-6b97-4cc6-bbde-87974f1bb678"
+	// Give us some more verbose debugging output
+	// Config = make(map[string]string)
+	// Config["detailed_card_info"] = "true"
+	for _, nums := range []struct {
+		a int
+	}{
+		{1}, {2}, {0}, {100},
+	} {
+		c := Card{name: "Test Card", uuid: uuid, plat: 1, gold: 1, rarity: "Epic", qty: 0, nature: "Smelly"}
+		cardCollection[uuid] = c
+		rawChangeCardCount(uuid, nums.a)
+		nc := cardCollection[uuid]
+		if nc.qty != nums.a {
+			t.Errorf("c.rawChangeCardCount(%v) == %v but we expected %v\n\tCARDINFO: %v", nums.a, nc.qty, nums.a, nc.InfoWithWheelInfo(0))
+		}
+
+	}
+}
+
+func TestRawChangeEACardCount(t *testing.T) {
+	uuid := "ff9f4b37-6b97-4cc6-bbde-87974f1bb678"
+	for _, nums := range []struct {
+		a int
+	}{
+		{1}, {2}, {0}, {100},
+	} {
+		c := Card{name: "Test Card", uuid: uuid, plat: 1, gold: 1, rarity: "Epic", qty: 0, eaqty: 0, nature: "Smelly"}
+		cardCollection[uuid] = c
+		rawChangeEACardCount(uuid, nums.a)
+		nc := cardCollection[uuid]
+		if nc.eaqty != nums.a {
+			t.Errorf("c.rawChangeCardCount(%v) == %v but we expected %v\nCARDINFO: %v\n", nums.a, nc.qty, nums.a, nc.InfoWithWheelInfo(0))
+		}
+	}
+}
+
+func TestEARawChangeCardCount(t *testing.T) {
+	uuid := "ff9f4b37-6b97-4cc6-bbde-87974f1bb678"
+	// Give us some more verbose debugging output
+	// Config = make(map[string]string)
+	// Config["detailed_card_info"] = "true"
+	for _, nums := range []struct {
+		a int
+	}{
+		{1}, {2}, {0}, {100},
+	} {
+		c := Card{name: "Test Card", uuid: uuid, plat: 1, gold: 1, rarity: "Epic", qty: 0, nature: "Smelly"}
+		cardCollection[uuid] = c
+		rawChangeCardCount(uuid, nums.a)
+		nc := cardCollection[uuid]
+		if nc.qty != nums.a {
+			t.Errorf("c.rawChangeCardCount(%v) == %v but we expected %v\n\tCARDINFO: %v", nums.a, nc.qty, nums.a, nc.InfoWithWheelInfo(0))
+		}
+
 	}
 }
